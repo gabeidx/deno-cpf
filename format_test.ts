cc89c3bc4809f1f88c0371cd0b265d05f5aeb46f
Gabriel Silva <gabriel.izaias@gmail.com>
2021-09-28 15:09:42 +0200

    update dev dependencies to latest versions

--- format_test.ts
+++ format_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from 'https://deno.land/std@0.86.0/testing/asserts.ts'
+import { assertEquals } from 'https://deno.land/std@0.108.0/testing/asserts.ts'
 
 import { format } from './format.ts'
 
